36ed839041fa9e24b602168e247e3b0c71ce2b70
Vadim Mychko <vadim_mychko@proton.me>
2024-03-25 23:09:03 +0100

    change obey robots.txt to false

--- sreality/sreality/settings.py
+++ sreality/sreality/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "sreality.spiders"
 #USER_AGENT = "sreality (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
