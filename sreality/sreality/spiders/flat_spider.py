5113009e339316c56f261bb13ba8cc1571ee78d6
Vadim Mychko <vadim_mychko@proton.me>
2024-03-25 23:09:37 +0100

    update link to scrape from

--- sreality/sreality/spiders/flat_spider.py
+++ sreality/sreality/spiders/flat_spider.py
@@ -1,16 +1,18 @@
-from pathlib import Path
+import json
 
-from scrapy import Spider
+from pathlib import Path
+from scrapy import Spider, Request
 
 
 class FlatSpider(Spider):
     name = "flats"
 
-    start_urls = [
-        "https://sreality.cz/en/search/for-sale/apartments"
-    ]
+    def start_requests(self):
+        url = "https://www.sreality.cz/api/en/v2/estates?page=1&per_page=500"
+        yield Request.from_curl(f"curl '{url}'")
 
     def parse(self, response):
-        filename = "temp.html"
-        Path(filename).write_bytes(response.body)
+        data = json.loads(response.text)
+        filename = "data.txt"
+        Path(filename).write_bytes(data)
         self.log(f"Saved file {filename}")
