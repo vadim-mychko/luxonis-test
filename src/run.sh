d4e4cce257fc47e0a6bfaf511f77edf237f1e5f4
Vadim Mychko <vadim_mychko@proton.me>
2024-03-26 07:03:02 +0100

    update entrypoint docker script

--- src/run.sh
+++ src/run.sh
@@ -7,10 +7,12 @@ export POSTGRES_HOST="db"
 export POSTGRES_DB="scrapy"
 export POSTGRES_PASSWORD="admin"
 
-until /app/src/wait-for-it.sh -h $POSTGRES_HOST -p 5432 -t 5; do
+cd /app/src
+
+until ./wait-for-it.sh -h $POSTGRES_HOST -p 5432 -t 5; do
     echo "Waiting for PostgreSQL..."
 done
 
-scrapy runspider /app/src/flat_spider.py
-python /app/src/update_database.py /app/scraped_data.json
-python /app/src/server.py
+scrapy runspider flat_spider.py
+python update_database.py scraped_data.json
+python server.py
