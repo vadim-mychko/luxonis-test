ca45bc41668b409ce978f74b35df9cdf86bb51dd
Vadim Mychko <vadim_mychko@proton.me>
2024-03-26 02:20:04 +0100

    update server to show all images from the database

--- src/server.py
+++ src/server.py
@@ -1,10 +1,45 @@
 import http.server
 import socketserver
+import psycopg2
+from jinja2 import Environment, FileSystemLoader
 
-PORT = 8080
+from config import DATABASE_CONFIG
 
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Serving at port", PORT)
-    httpd.serve_forever()
+PORT = 8082
+ENV = Environment(loader=FileSystemLoader("."))
+
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/":
+            self.respond()
+        else:
+            super().do_GET()
+  
+    def respond(self):
+        conn = psycopg2.connect(**DATABASE_CONFIG)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM data")
+        rows = cur.fetchall()
+
+        template = ENV.get_template("index.html")
+        content = template.render(data=rows)
+        
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(content, "utf-8"))
+
+        cur.close()
+        conn.close()
+
+
+def main():
+    with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+        httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
