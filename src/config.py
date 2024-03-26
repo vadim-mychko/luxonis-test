da30f2e961aa8b585ae2371fea1489a7853eb145
Vadim Mychko <vadim_mychko@proton.me>
2024-03-26 04:08:21 +0100

    update config

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 DATABASE_CONFIG = dict(
     database=os.environ.get("POSTGRES_DB"),
     user=os.environ.get("POSTGRES_USER"),
-    password=os.environ.get("POSTGRES_DB"),
+    password=os.environ.get("POSTGRES_PASSWORD"),
     host="localhost",
     port="5432"
 )
